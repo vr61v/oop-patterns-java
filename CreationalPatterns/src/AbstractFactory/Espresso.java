9b72891d2a3066e2f1e1889fd182ce4597346877
Vr61v <nikita20040503@gmail.com>
2024-01-28 15:23:46 +0300

    refactor: espresso in Abstract factory
    Change type espresso class to abstract class

--- CreationalPatterns/src/AbstractFactory/Espresso.java
+++ CreationalPatterns/src/AbstractFactory/Espresso.java
@@ -1,6 +1,6 @@
 package AbstractFactory;
 
-public class Espresso extends Coffee {
+public abstract class Espresso extends Coffee {
     protected final boolean COFFEE = true;
     protected final boolean MILK = false;
     protected final boolean FOAM = false;
